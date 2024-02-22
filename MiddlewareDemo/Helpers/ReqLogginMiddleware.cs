fed4c10be01539a962f9c850616d751467c9553f
DivyeshMakwana <dmakwana@toastmasters.org>
2024-02-23 00:04:26 +0530

    Initail Commits

---
+++ MiddlewareDemo/Helpers/ReqLogginMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace MiddlewareDemo.Helpers
+{
+    public class ReqLogginMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ReqLogginMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context) { 
+
+            //log each request
+            Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+
+            // Add an extra request parameter
+            context.Request.QueryString = context.Request.QueryString.Add("newparam", "newValue");
+
+            _next(context);
+        }
+    }
+}
